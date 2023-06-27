cbb16b38fa21823df5a86efd92e9e14cb1c7b337
kitaminka <kit4minka@gmail.com>
2023-06-27 16:07:41 +0300

    Update report command

--- interactions/commands.go
+++ interactions/commands.go
@@ -2,7 +2,6 @@ package interactions
 
 import (
 	"github.com/bwmarrin/discordgo"
-	"github.com/kitaminka/discord-bot/cfg"
 	"log"
 )
 
@@ -11,19 +10,22 @@ type Command struct {
 	Handler            func(session *discordgo.Session, interactionCreate *discordgo.InteractionCreate)
 }
 
-var Commands = map[string]Command{
-	"Отправить репорт": {
-		ApplicationCommand: &discordgo.ApplicationCommand{
-			Type: discordgo.MessageApplicationCommand,
-			Name: "Отправить репорт",
+var (
+	Commands = map[string]Command{
+		"Отправить репорт": {
+			ApplicationCommand: &discordgo.ApplicationCommand{
+				Type:         discordgo.MessageApplicationCommand,
+				Name:         "Отправить репорт",
+				DMPermission: new(bool),
+			},
+			Handler: reportMessageCommandHandler,
 		},
-		Handler: reportMessageCommandHandler,
-	},
-}
+	}
+)
 
 func CreateApplicationCommands(session *discordgo.Session) {
 	for index, value := range Commands {
-		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, cfg.Config.ServerID, value.ApplicationCommand)
+		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, "", value.ApplicationCommand)
 		if err != nil {
 			log.Panicf("Error creating '%v' command: %v", value.ApplicationCommand.Name, err)
 		}
@@ -36,13 +38,13 @@ func CreateApplicationCommands(session *discordgo.Session) {
 	}
 }
 func RemoveApplicationCommands(session *discordgo.Session) {
-	commands, err := session.ApplicationCommands(session.State.User.ID, cfg.Config.ServerID)
+	commands, err := session.ApplicationCommands(session.State.User.ID, "")
 	if err != nil {
 		log.Printf("Error getting application commands: %v", err)
 		return
 	}
 	for _, command := range commands {
-		err = session.ApplicationCommandDelete(session.State.User.ID, cfg.Config.ServerID, command.ID)
+		err = session.ApplicationCommandDelete(session.State.User.ID, "", command.ID)
 		if err != nil {
 			log.Panicf("Error deleting '%v' command: %v", command.Name, err)
 		}
