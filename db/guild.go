90d9208925434e1dcb83731eb04e219d0deb6354
kitaminka <kit4minka@gmail.com>
2023-07-04 19:15:14 +0300

    Update GuildsCollectionName

--- db/guild.go
+++ db/guild.go
@@ -5,6 +5,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const GuildsCollectionName = "guilds"
+
 type Guild struct {
 	ID                     string `bson:"id,omitempty"`
 	ReportChannelID        string `bson:"reportChannelId,omitempty"`
@@ -12,12 +14,12 @@ type Guild struct {
 }
 
 func UpdateGuild(server Guild) error {
-	_, err := MongoDatabase.Collection("servers").UpdateOne(nil, bson.D{{"id", server.ID}}, bson.D{{"$set", server}}, options.Update().SetUpsert(true))
+	_, err := MongoDatabase.Collection(GuildsCollectionName).UpdateOne(nil, bson.D{{"id", server.ID}}, bson.D{{"$set", server}}, options.Update().SetUpsert(true))
 	return err
 }
 
 func GetGuild(discordId string) (Guild, error) {
 	var server Guild
-	err := MongoDatabase.Collection("servers").FindOne(nil, bson.D{{"id", discordId}}).Decode(&server)
+	err := MongoDatabase.Collection(GuildsCollectionName).FindOne(nil, bson.D{{"id", discordId}}).Decode(&server)
 	return server, err
 }
